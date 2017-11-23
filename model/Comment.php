d39610a678602f5a0d74fca9e40d8a7d4cd9eaa3
PARHELIA 3 <parhelia3@iMac-de-PARHELIA-2.local>
2017-11-23 17:23:14 +0100

    Creation des getter et setter dans les class Post et Comment

--- model/Comment.php
+++ model/Comment.php
@@ -4,5 +4,99 @@ namespace JeanForteroche\Blog\Model;
 
 class Comment {
 
+    private $_id;
+    private $_postId;
+    private $_author;
+    private $_comment;
+    private $_commentDate;
+    private $_alert;
+
+    // SETTER
+    public function setId($id){
+        if(!isint($id)){
+            throw new Exception('Erreur : l\'id utilisé n\'est pas un entier');
+            return;
+        }
+        if($id<1){
+            throw new Exception('Erreur : l\'id utilisé doit etre positif');
+            return;
+        }
+
+        $this->_id = $id ;
+    }
+    
+    public function setPostId($postId){
+        if(!isint($postId)){
+            throw new Exception('Erreur : l\'id utilisé n\'est pas un entier');
+            return;
+        }
+        if($postId<1){
+            throw new Exception('Erreur : l\'id utilisé doit etre positif');
+            return;
+        }
+        
+        $this->_postId = $postId ;
+    }
+    
+    public function setAuthor($author){
+        if(!is_string($author)){
+            throw new Exception('Erreur : le nom de l\'auteur utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_author = $author ;
+    }
+    
+    public function setComment($comment){
+        if(!is_string($comment)){
+            throw new Exception('Erreur : le contenu utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_comment = $comment ;
+    }
+    
+    public function setCommentDate($date){
+        if(!is_string($date)){
+            throw new Exception('Erreur : le titre utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_commentDate = $date ;
+    }
+    
+    public function setAlert($alert){
+        if(!isint($alert)){
+            throw new Exception('Erreur : la valeur utilisé n\'est pas un entier');
+            return;
+        }
+        if($alert<1){
+            throw new Exception('Erreur : la valeur utilisé doit etre positif');
+            return;
+        }
+        
+        $this->_alert = $alert ;
+    }
+    
+
+    // GETTER
+    public function id(){
+        return $this->_id;
+    }
+    public function postId(){
+        return $this->_postId;
+    }
+    public function author(){
+        return $this->_author;
+    }
+    public function comment(){
+        return $this->_comment ;
+    }
+    public function commentDate(){
+        return $this->_commentDate;
+    }
+    public function alert(){
+        return $this->_alert;
+    }
 
 }
--- model/Post.php
+++ model/Post.php
@@ -4,5 +4,94 @@ namespace JeanForteroche\Blog\Model;
 
 class Post {
 
+    private $_id;
+    private $_title;
+    private $_chapter;
+    private $_content;
+    private $_creation_date;
+    private $_img;
 
+    // SETTER
+    public function setId($id){
+        if(!isint($id)){
+            throw new Exception('Erreur : l\'id utilisé n\'est pas un entier');
+            return;
+        }
+        if($id<1){
+            throw new Exception('Erreur : l\'id utilisé doit etre positif');
+            return;
+        }
+
+        $this->_id = $id ;
+    }
+    
+    public function setTitle($title){
+        if(!is_string($title)){
+            throw new Exception('Erreur : le titre utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_title = $title ;
+    }
+    
+    public function setChapter($chapter){
+        if(!isint($chapter)){
+            throw new Exception('Erreur : le numero de chapitre utilisé n\'est pas un entier');
+            return;
+        }
+        if($chapter<1){
+            throw new Exception('Erreur : le numero de chapitre doit etre positif');
+            return;
+        }
+        
+        $this->_chapter = $chapter ;
+    }
+    
+    public function setContent($content){
+        if(!is_string($content)){
+            throw new Exception('Erreur : le contenu utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_content = $content ;
+    }
+    
+    public function setCreationDate($date){
+        if(!is_string($date)){
+            throw new Exception('Erreur : le titre utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_creation_date = $date ;
+    }
+    
+    public function setImg($img){
+        if(!is_string($img)){
+            throw new Exception('Erreur : le titre utilisé n\'est pas du type string');
+            return;
+        }
+        
+        $this->_img = $img ;
+    }
+    
+
+    // GETTER
+    public function id(){
+        return $this->_id;
+    }
+    public function title(){
+        return $this->_title;
+    }
+    public function chapter(){
+        return $this->_chapter;
+    }
+    public function content(){
+        return $this->_content ;
+    }
+    public function creation_date(){
+        return  $this->_creation_date;
+    }
+    public function img(){
+        return  $this->_img;
+    }
 }
